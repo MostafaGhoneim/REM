4f8173410b934deccea19196a944ff73c0a0d318
MostafaGhoneim <102383018+MostafaGhoneim@users.noreply.github.com>
2023-02-27 18:11:50 +0200

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+     //   System.setProperty("webdriver.chrome.driver","C:\\Users\\mostafa.ghoneim\\Downloads\\chrome\\chromedriver.exe");
+    //    WebDriver obj = new ChromeDriver();
+     //   obj.get("http://www.google.com");
+
+
+    }
+}
